841a0e7afc612205dfc6c293651c2aeaf148d91c
Potatocat7 <potatocat7@gmail.com>
2019-11-29 06:07:33 +0900

    Timer
    時計表示作成

---
+++ Assets/Scripts/ClockScript.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClockScript : MonoBehaviour
+{
+    private Text ClockTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ClockTimer = GetComponentInChildren<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ClockTimer.text = DateTime.Now.ToShortTimeString();
+    }
+}
