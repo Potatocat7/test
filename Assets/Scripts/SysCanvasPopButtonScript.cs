7229dca2a55c36b0264dbe854c055b2b270ced33
Potatocat7 <potatocat7@gmail.com>
2019-11-30 07:44:05 +0900

    popwindow
    ポップウィンドウ（時間設定用）と停止用ボタンを追加
    ボタンで表示/非表示をできるように修正

---
+++ Assets/Scripts/SysCanvasPopButtonScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SysCanvasPopButtonScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //ボタン押下時
+    public void OnClick()
+    {
+        GameObject.Find("StartScene").transform.Find("SysWindowCanvas").gameObject.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/SysResetScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SysResetScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //ボタン押下時
+    public void OnClick()
+    {
+        GameObject.Find("SysWindowCanvas").SetActive(false);
+    }
+}
