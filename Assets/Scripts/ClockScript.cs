14b09948515d69a94da8ad5c0137fd1ecb1fda6c
Potatocat7 <potatocat7@gmail.com>
2019-11-30 17:52:26 +0900

    SetTimer_start
    セット時に設定した数値を表示するように変更

--- Assets/Scripts/ClockScript.cs
+++ Assets/Scripts/ClockScript.cs
@@ -6,11 +6,12 @@ using UnityEngine.UI;
 
 public class ClockScript : MonoBehaviour
 {
+    public int status;
     private Text ClockTimer;
-
     // Start is called before the first frame update
     void Start()
     {
+        status = 0;
         ClockTimer = GetComponentInChildren<Text>();
     }
 
--- Assets/Scripts/SysSetScript.cs
+++ Assets/Scripts/SysSetScript.cs
@@ -1,15 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SysSetScript : MonoBehaviour
 {
+    //public DateTime SetdataTime;
+    private Text SetClockTimer;
+    private InputField SetClockHour;
+    private InputField SetClockMinutes;
     private Button StopButton;
     // Start is called before the first frame update
     void Start()
     {
         StopButton = GameObject.Find("StopButton").GetComponent<Button>();
+        SetClockTimer = GameObject.Find("SetTimeCheck").GetComponent<Text>();       
+        SetClockHour = GameObject.Find("TimeHour").GetComponent<InputField>();
+        SetClockMinutes = GameObject.Find("TimeMinutes").GetComponent<InputField>();
     }
 
     // Update is called once per frame
@@ -22,7 +30,7 @@ public class SysSetScript : MonoBehaviour
     {
         StopButton.enabled = false;
         StopButton.interactable = false;
-        GameObject.Find("SetTimeCheck").GetComponent<Text>().text = "設定";
+        SetClockTimer.text = SetClockHour.text + ":" + SetClockMinutes.text;
         GameObject.Find("SysWindowCanvas").SetActive(false);
     }
 }
